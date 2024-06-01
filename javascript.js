a883df27c4bf49e22b97a6e8ef9d77c742d4d107
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-01 23:12:32 +0200

    Create player function and initial game validation
    Create player factory function that tracks name
    and wins.
    
    Create a function within GameBoard that checks
    horizontal and vertical lines for wins

--- javascript.js
+++ javascript.js
@@ -6,39 +6,90 @@ const GameBoard = (function() {
     ];
 
     function printBoard() {
-        for (let x in gameBoard){
-            console.log(gameBoard[x]);
+        for (let row in gameBoard){
+            console.log(gameBoard[row]);
         };
     };
 
-    function setCell(x, y, char) {
+    function setCell(row, col, char) {
         if (char !== 'X' && char !== 'O') {
-            console.log(`Error: setCell(x, y, char). Char is ${char}`);
+            console.log(`Error: setCell(row, col, char). Char is ${char}`);
             return;
         }
-        if (![0, 1, 2].includes(x) || ![0, 1, 2].includes(y)) {
-            console.log(`Error: setCell(x, y, char). x or y is out of bounds`);
+        if (![0, 1, 2].includes(row) || ![0, 1, 2].includes(col)) {
+            console.log(`Error: setCell(row, col, char). Row or col is out of bounds`);
             return;
         }
-        gameBoard[x][y] = char;
-        printBoard();
+        gameBoard[row][col] = char;
     };
+
+    function checkGameState() {
+
+        // Horizontal
+        for (let row of gameBoard) {
+            const result = checkWin(row);
+            if (result) {
+                return row[0];
+            }
+        }
+
+        // Vertical
+        for (let col = 0; col < 3; col++) {
+            const ColToCheck = gameBoard.map(row => row[col]);
+            const result = checkWin(ColToCheck);
+            if (result) {
+                return ColToCheck[0];
+            }
+        }
+
+        return false;
+    }
+
+    function checkWin(row) {
+        return row.every(value => value === "O") || 
+                row.every(value => value === "X");
+    }
+
 
 
     return {
         gameBoard,
         setCell,
-        printBoard
+        printBoard,
+        checkGameState
     }
 })();
 
 
 function createPlayer(name) {
-    const player = {};
-    player.name = name;
-    player.won = 0;
+    let wins = 0;
 
-    return player;
+    function getWins() {
+        return wins;
+    }
+    
+    function increaseWins() {
+        wins++;
+    }
+
+    function resetWins() {
+        wins = 0
+    }
+
+    function checkWon() {
+        return (wins === 3);
+    }
+
+    return {name, getWins, increaseWins, resetWins, checkWon};
 }
 
-GameBoard.setCell(4, 2, "O");
+const Game = (function(){
+    const player1 = createPlayer('Stijn');
+    const player2 = createPlayer('Eva');
+    GameBoard.setCell(2, 0, 'O');
+    GameBoard.setCell(2, 1, 'O');
+    GameBoard.setCell(2, 2, 'O');
+    GameBoard.printBoard();
+    console.log(GameBoard.checkGameState());
+
+})();
