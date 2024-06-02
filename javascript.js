7925ecf51f8c5ad4f07b6b109693cf32e82c5216
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-06-02 17:32:15 +0200

    Add renderer object with eventlisteners

--- javascript.js
+++ javascript.js
@@ -1,18 +1,80 @@
-// const Game = require('./scripts/game.js');
 import Game from './scripts/game.js';
 
-// Game.gameLoop();
 
-document.addEventListener("DOMContentLoaded",() => {
+
+
+
+
+// Renderer
+//
+//
+
+
+const Renderer = (function() {
     const cells = document.querySelectorAll('.grid-cell');
-    cells.forEach((cell) => {
-        cell.addEventListener("mouseenter", () => {
-            cell.innerHTML = "X";
+    let symbol = "X";
+
+    function setSymbol(toSymbol) {
+        console.log("setSymbol called");
+        symbol = toSymbol;
+        removeListeners(); // Remove existing listeners for the previous symbol
+        setListeners();    // Set new event listeners
+    };
+
+
+
+
+    // Set and remove listener functions for click and hover
+    function setListeners() {
+        cells.forEach((cell) => {
+            cell.addEventListener("click", (event) => cellClick(event));
+            cell.addEventListener("mouseenter", (event) => cellMouseEnter(event));
+            cell.addEventListener("mouseleave", (event) => cellMouseLeave(event));
         });
-    
-        cell.addEventListener("mouseleave", () => {
-            cell.innerHTML = "";
+    };
+
+    function removeListeners() {
+        cells.forEach((cell) => {
+            cell.removeEventListener("click", (event) => cellClick(event));
+            cell.removeEventListener("mouseenter", (event) => cellMouseEnter(event));
+            cell.removeEventListener("mouseleave", (event) => cellMouseLeave(event));
         })
-    })   
-})
-   
+    }
+
+    function cellClick(event) {
+        const cell = event.currentTarget;
+        if (cell.classList.contains("cell-hover")) {
+            cell.textContent = symbol;
+            cell.classList.remove("cell-hover");
+        };
+    }
+
+    function cellMouseEnter(event) {
+        const cell = event.currentTarget;
+        if (!cell.textContent) {
+            cell.classList.add("cell-hover");
+            cell.textContent = symbol;
+        };
+    }
+
+    function cellMouseLeave(event) {
+        const cell = event.currentTarget;
+        if (cell.classList.contains("cell-hover")) {
+            cell.textContent = "";
+            cell.classList.remove("cell-hover");
+        };
+    }
+    
+
+
+    
+
+    return {
+        setSymbol,
+    }
+})();
+
+
+
+Renderer.setSymbol("O");
+Renderer.setSymbol("L")
